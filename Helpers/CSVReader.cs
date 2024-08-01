fdc064739aa2974e3f75f034e23a593923f8105a
Кирилл Солдатенко <soldatenko.kirillas@gmail.com>
2024-08-01 20:46:18 +0300

    feat: reworked csv reader

--- Helpers/CSVReader.cs
+++ Helpers/CSVReader.cs
@@ -1,11 +1,9 @@
-﻿using CsvHelper;
-using CsvHelper.Configuration;
-using CSVReaderTask.Helpers.Interfaces;
+﻿using CSVReaderTask.Helpers.Interfaces;
 using CSVReaderTask.Models;
-using CSVReaderTask.Models.CsvMap;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
+using System.Text;
 
 namespace CSVReaderTask.Helpers
 {
@@ -15,8 +13,10 @@ namespace CSVReaderTask.Helpers
     /// </summary>
     public class CSVReader : ICSVReader
     {
-        private const int BunchReturnCount = 2048;
-        private const string CSVDelimiter = ";";
+        private const int BunchReturnCount = 4096;
+        private const char CSVDelimiter = ';';
+        const int expectedParts = 6;
+        string[] parts = new string[expectedParts];
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CSVReader"/> class.
@@ -31,40 +31,69 @@ namespace CSVReaderTask.Helpers
         /// <exception cref="IOException">Thrown when an error occurs during file reading.</exception>
         public async IAsyncEnumerable<IEnumerable<Person>> ReadFilePersonAsync(string filePath)
         {
-            using (var reader = new StreamReader(filePath))
-            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false,
-                Delimiter = CSVDelimiter,
-                BadDataFound = context =>
-                {
-                    throw new Exception($"Bad data found : {context.RawRecord}");
-                },
-                MissingFieldFound = context =>
-                {
-                    throw new Exception($"Missing field  on row {context.Index}");
-                }
-            }))
-            {
-                csv.Context.RegisterClassMap<PersonMap>();
+            Debug.WriteLine("Start reading");
+            var startTime = DateTime.Now;
 
-                var records = csv.GetRecordsAsync<Person>();
-                var personList = new List<Person>();
-                await foreach (var record in records)
+            var personList = new List<Person>();
+
+            using var streamReader = new StreamReader(filePath, Encoding.UTF8);
+            while (!streamReader.EndOfStream)
+            {
+                var line = await streamReader.ReadLineAsync();
+                if (!line.IsNullOrEmpty())
                 {
-                    personList.Add(record);
-                    if (personList.Count > BunchReturnCount)
+                    var person = ParseLine(line);
+                    personList.Add(person);
+
+                    if (personList.Count >= BunchReturnCount)
                     {
-                        Debug.WriteLine("Send bunch");
                         yield return personList;
                         personList = new List<Person>();
                     }
                 }
-                if (personList.Count > 0)
-                    yield return personList;
-
             }
+            if (personList.Count > 0)
+            {
+                yield return personList;
+            }
+            Debug.WriteLine($"Time elapsed {DateTime.Now - startTime}");
         }
 
+        private Person ParseLine(string line)
+        {
+            var span = line.AsSpan();
+
+            int start = 0;
+            int partIndex = 0;
+
+            while (partIndex < expectedParts)
+            {
+                int delimiterIndex = span.Slice(start).IndexOf(CSVDelimiter);
+
+                if (delimiterIndex == -1)
+                {
+                    delimiterIndex = span.Length - start;
+                }
+
+                parts[partIndex++] = span.Slice(start, delimiterIndex).ToString();
+
+                start += delimiterIndex + 1;
+
+                if (start >= span.Length && partIndex < expectedParts)
+                {
+                    throw new ArgumentException("The line does not contain enough data to create a Person object.");
+                }
+            }
+
+            return new Person
+            {
+                Date = DateTime.Parse(parts[0]),
+                FirstName = parts[1],
+                LastName = parts[2],
+                SurName = parts[3],
+                City = parts[4],
+                Country = parts[5]
+            };
+        }
     }
 }
